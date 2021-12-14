7155d07f71ee9e2ccca85cc8d010db39f97e6b8a
Alannegra <alanrodriguez@elpuig.xeill.net>
2021-12-14 02:07:40 +0100

    Costurero ejercicio

--- src/com/company/Costurero.java
+++ src/com/company/Costurero.java
@@ -5,9 +5,6 @@ public class Costurero extends Thread {
     String nombre;
     Recursos recurso;
 
-
-    //p.ej.#que#son#dos#constantes#enteras#"NumMaxMangas"#y#"NumMaxCuerpos").
-
     public Costurero(String nombre, Recursos recurso) {
         this.nombre = nombre;
         this.recurso = recurso;
@@ -17,44 +14,22 @@ public class Costurero extends Thread {
     public void run() {
 
         for(;;) {
-            //Agafa el comandament
-
 
                 if(nombre.equals("manga")){
                     recurso.añadirManga();
-
-
             }
 
 
                 if(nombre.equals("cuerpo")){
                     recurso.añadirCuerpo();
-
-
             }
 
-            if(true){
+
                 if(nombre.equals("ensamblador")){
                     recurso.añadirJersei();
-                    //jersey++;
-                }
             }
 
-            /*synchronized (lock){
 
-                try {
-                    cubierto1.agafa();
-                    Thread.sleep((long) (Math.random()*400)+200);
-
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-
-                cubierto2.agafa();
-            }*/
-
-
-            //System.out.println(nombre + " " + cubierto1.getNombre() + " " + cubierto2.getNombre());
             System.out.println("En el " + recurso.getNombre() + " añade " +nombre + " M" + recurso.mangas + " C" + recurso.cuerpos + " J" + recurso.jerseis);
 
             try {
@@ -63,15 +38,9 @@ public class Costurero extends Thread {
                 e.printStackTrace();
             }
 
-            //Deixa el comandament
             recurso.deixa();
-            //cubierto2.deixa();
-            //System.out.println(nombre + " " + cubierto1.getNombre() + " " + cubierto2.getNombre());
 
-            //Descansa de tanta tele
             try {
-                //System.out.println(getName() + " ja no miro la tele");
-                //System.out.format("%s %s %n", getName(),resourceBundle.getString("persona.deixar_tv"));
                 Thread.sleep((long) (Math.random()*1000)+500);
             } catch (InterruptedException e) {
                 e.printStackTrace();
--- src/com/company/Recursos.java
+++ src/com/company/Recursos.java
@@ -22,17 +22,6 @@ public class Recursos {
         lliure = true;
     }
 
-    public synchronized void agafa() {
-
-            try {
-                while (!lliure) wait();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            lliure = false;
-            notifyAll();
-
-    }
 
     public synchronized void añadirManga() {
         if(NumMaxMangas > mangas) {
